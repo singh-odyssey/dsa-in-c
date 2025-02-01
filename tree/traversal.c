71cd4ffbeaf5ff9d93ab979b25762602b87bc933
AdityaSejwar <148507838+AdityaSejwar@users.noreply.github.com>
2025-02-01 18:18:59 +0000

    Add trav_tree

---
+++ tree/traversal.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+void preOrder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    printf("%d ", root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+int main()
+{
+    struct node *root = createNode(10);
+    root->left = createNode(20);
+    root->right = createNode(30);
+    root->left->left = createNode(40);
+    root->left->right = createNode(50);
+    preOrder(root);
+    printf("\n");
+
+
+    return 0;
+}
